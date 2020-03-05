7b682d43ad1483812554d6c14c50441ae99ffb71
Jordan Sosnowski <jjs0029@auburn.edu>
2020-03-05 16:27:50 -0600

    Create header file for aubatch

--- aubatch.c
+++ aubatch.c
@@ -15,74 +15,7 @@
  *
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <limits.h>
-
-typedef unsigned int u_int;
-
-#define CMD_BUF_SIZE 10 /* The size of the command queue */
-#define NUM_OF_CMD 5    /* The number of submitted jobs   */
-#define MAX_CMD_LEN 512 /* The longest scheduler length */
-
-enum scheduling_policies
-{
-    FCFS,
-    SJF,
-    PRIORITY,
-} policy;
-
-typedef struct
-{
-    char cmd[MAX_CMD_LEN];
-    time_t arrival_time;
-    int cpu_burst;
-    int cpu_remaining_burst;
-    int priority;
-    int interruptions;
-    int first_time_on_cpu;
-
-} process_t;
-
-typedef struct
-{
-    char cmd[MAX_CMD_LEN];
-    time_t arrival_time;
-    int cpu_burst;
-    int first_time_on_cpu;
-    int priority;
-    int interruptions;
-    int finish_time;
-    int turnaround_time;
-    int waiting_time;
-    int response_time;
-
-} finished_process_t;
-
-typedef process_t *process_p;
-typedef finished_process_t *finished_process_p;
-
-void *scheduler(void *ptr);  /* To simulate job submissions and scheduling */
-void *dispatcher(void *ptr); /* To simulate job execution */
-
-void sort_buffer(process_p *process_buffer);          /* sorts process buffer depending on scheduler */
-int sjf_scheduler(const void *a, const void *b);      /* sorts buffer by remaining cpu burst */
-int fcfs_scheduler(const void *a, const void *b);     /* sorts buffer by arrival time */
-int priority_scheduler(const void *a, const void *b); /* sorts buffer by priority */
-
-process_p get_process();
-process_p get_process_from_file(char *filename, int index);
-int run_process(int burst);               /* sleeps for burst seconds */
-void complete_process(process_p process); /* copys process to completed process buffer */
-
-void report_metrics(); /* loops through completed process buffer and prints metrics */
-
-char *convert_time(time_t time);   /* convers from epoch time to human readable string */
-void remove_newline(char *buffer); /* pulls newline off of string read from user input*/
+#include "aubatch.h"
 
 pthread_mutex_t cmd_queue_lock;   /* Lock for critical sections */
 pthread_cond_t cmd_buf_not_full;  /* Condition variable for buf_not_full */
@@ -99,7 +32,7 @@ int from_file;
 process_p process_buffer[CMD_BUF_SIZE];
 finished_process_p finished_process_buffer[8192];
 
-int main()
+int main(int argc, char **argv)
 {
     srand(time(NULL));
     pthread_t scheduling_thread, dispatching_thread; /* Two concurrent threads */
