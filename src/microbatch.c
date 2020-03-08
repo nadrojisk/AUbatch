b3429256f7a6dc20d13ba8b1d5eb8b40ae295675
Jordan Sosnowski <jjs0029@auburn.edu>
2020-03-08 13:54:28 -0500

    Update comments on microbatch

--- src/microbatch.c
+++ src/microbatch.c
@@ -1,3 +1,22 @@
+/* 
+ * COMP7500/7506
+ * Project 3: microbatch 
+ * 
+ * Author: Jordan Sosnowski
+ * Reference: Dr. Xiao Qin
+ * 
+ * Date: March 9, 2020. Version 1.0
+ *
+ * Example program to be called by aubatch, simply sleeps for argv[2] time
+ * This assumes argv[2] is a number
+ * 
+ * If you were to call ./microbatch 10 it would simply sleep for 10 seconds
+ * 
+ * Compilation Instruction: 
+ * gcc -o ./microbatch.out ./microbatch.c
+ *
+ */
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
