93a4b2c78e9d3c704d4c1b96a0d5dbe4e3b6603c
Jordan Sosnowski <jjs0029@auburn.edu>
2020-03-08 11:49:33 -0500

    Update aubatch
    Update comments and remove redundant variables

--- src/aubatch.c
+++ src/aubatch.c
@@ -7,48 +7,35 @@
  * 
  * Date: March 9, 2020. Version 1.0
  *
- * This sample source code demonstrates the development of 
- * a batch-job scheduler using pthread.
+ * The main driver for aubatch, sets up all the necessary variables
+ * for the program to work properly. Creates two threads, executor and disaptcher
  *
  * Compilation Instruction: 
- * gcc aubatch.c -o aubatch -lpthread
+ * gcc -o ./aubatch.out ./aubatch.c ./commandline.c ./modules.c -lpthread -Wall
  *
  */
 
 #include "commandline.h"
 
-/* Global shared variables */
-u_int buf_head;
-u_int buf_tail;
-u_int count;
-u_int finished_head;
-
-int from_file;
-
-process_p process_buffer[CMD_BUF_SIZE];
-finished_process_p finished_process_buffer[8192];
-
 int main(int argc, char **argv)
 {
     printf("Welcome to Jordan Sosnowski's batch job scheduler Version 1.0.\nType ‘help’ to find more about AUbatch commands.\n");
-    pthread_t executor_thread, dispatching_thread; /* Two concurrent threads */
-    char *message1 = "Executor Thread";
-    char *message2 = "Dispatching Thread";
+    pthread_t executor_thread, dispatcher_thread; /* Two concurrent threads */
+
     int iret1, iret2;
 
-    policy = FCFS; // policy for scheduler
-    // from_file = 1;
+    policy = FCFS; // default policy for scheduler
 
-    /* Initialize count, two buffer pointers */
+    /* Initialize count, three buffer pointers */
     count = 0;
     buf_head = 0;
     buf_tail = 0;
     finished_head = 0;
 
-    /* Create two independent threads:command and dispatchers */
+    /* Create two independent threads: executor and dispatcher */
 
-    iret1 = pthread_create(&executor_thread, NULL, commandline, (void *)message1);
-    iret2 = pthread_create(&dispatching_thread, NULL, dispatcher, (void *)message2);
+    iret1 = pthread_create(&executor_thread, NULL, commandline, (void *)NULL);
+    iret2 = pthread_create(&dispatcher_thread, NULL, dispatcher, (void *)NULL);
 
     /* Initialize the lock the two condition variables */
     pthread_mutex_init(&cmd_queue_lock, NULL);
@@ -59,11 +46,11 @@ int main(int argc, char **argv)
     /* wait we run the risk of executing an exit which will terminate   */
     /* the process and all threads before the threads have completed.   */
     pthread_join(executor_thread, NULL);
-    pthread_join(dispatching_thread, NULL);
+    pthread_join(dispatcher_thread, NULL);
 
     if (iret1)
         printf("executor_thread returns: %d\n", iret1);
     if (iret2)
-        printf("dispatching_thread returns: %d\n", iret1);
-    exit(0);
+        printf("dispatcher_thread returns: %d\n", iret1);
+    return 0;
 }
