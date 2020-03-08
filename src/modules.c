da5f0170bfb35a544f659c5a5f2d4a3bbdd81d04
Jordan Sosnowski <jjs0029@auburn.edu>
2020-03-08 13:52:44 -0500

    Update modules

--- src/modules.c
+++ src/modules.c
@@ -1,41 +1,43 @@
 /* 
  * COMP7500/7506
- * Project 3: AUbatch 
+ * Project 3: modules 
  * 
  * Author: Jordan Sosnowski
  * Reference: Dr. Xiao Qin
  * 
  * Date: March 9, 2020. Version 1.0
  *
- * This sample source code demonstrates the development of 
- * a batch-job scheduler using pthread.
+ * Provides implemenation for the scheduling module and the dispatching module
  *
  * Compilation Instruction: 
- * gcc aubatch.c -o aubatch -lpthread
+ * gcc -o aubatch.out aubatch.c commandline.c modules.c -lpthread -Wall
  *
  */
 
 #include "modules.h"
 
-/* Global shared variables */
-
-process_p running_process;
-
+/*
+ * This function takes in arguments from command line when users select test
+ * 
+ * Simulates a benchmark against a certain scheduling algorithm which is picked in command line
+ * Can take in a number of jobs at once and can load them all at once
+ * 
+ * If arrival rate is > 0 then the jobs are loaded, dispatcher is notified and then we sleep for arrival_time seconds
+ * After sleeping we load the next job, if there is one
+ */
 void test_scheduler(char *benchmark, int num_of_jobs, int arrival_rate, int priority_levels, int min_CPU_time, int max_CPU_time)
 {
-    // TODO check if queue is empty first
     /* lock the shared command queue */
     pthread_mutex_lock(&cmd_queue_lock);
-
-    // printf("In scheduler: count = %d\n", count);
 
     while (count == CMD_BUF_SIZE)
     {
         pthread_cond_wait(&cmd_buf_not_full, &cmd_queue_lock);
     }
 
-    pthread_mutex_unlock(&cmd_queue_lock); //uncomment if you want the dispatcher to run while scheduler is loading
+    pthread_mutex_unlock(&cmd_queue_lock);
 
+    // create jobs based on num_of_jobs
     for (int i = 0; i < num_of_jobs; i++)
     {
         int priority = (rand() % (priority_levels + 1)) + 1;
@@ -65,7 +67,7 @@ void test_scheduler(char *benchmark, int num_of_jobs, int arrival_rate, int prio
 
             pthread_cond_signal(&cmd_buf_not_empty);
             pthread_mutex_unlock(&cmd_queue_lock);
-            sleep(arrival_rate);
+            sleep(arrival_rate); // wait for the arrival rate
         }
     }
     if (!arrival_rate) // if arrival rate is 0, load all the jobs and then notify dispatcher
@@ -81,31 +83,24 @@ void test_scheduler(char *benchmark, int num_of_jobs, int arrival_rate, int prio
     }
 }
 /* 
- * This function simulates a terminal where users may 
- * submit jobs into a batch processing queue.
- * Note: The input parameter (i.e., *ptr) is optional. 
- * If you intend to create a thread from a function 
- * with input parameters, please follow this example.
+ * This function takes in arguments from command line when users select run
+ * 
+ * Takes in one job / process at a time and notifies dispatcher there is a new job
  */
-
 void scheduler(int argc, char **argv)
 {
     /* lock the shared command queue */
     pthread_mutex_lock(&cmd_queue_lock);
-
-    // printf("In scheduler: count = %d\n", count);
 
     while (count == CMD_BUF_SIZE)
     {
         pthread_cond_wait(&cmd_buf_not_full, &cmd_queue_lock);
     }
 
-    pthread_mutex_unlock(&cmd_queue_lock); //uncomment if you want the dispatcher to run while scheduler is loading
+    pthread_mutex_unlock(&cmd_queue_lock);
     process_buffer[buf_head] = get_process(argv);
 
-    pthread_mutex_lock(&cmd_queue_lock); //uncomment if you want the dispatcher to run while scheduler is loading
-
-    // printf("In scheduler: process_buffer[%d] = %s\n", buf_head, process_buffer[buf_head]->cmd);
+    pthread_mutex_lock(&cmd_queue_lock);
 
     count++;
 
@@ -113,90 +108,19 @@ void scheduler(int argc, char **argv)
     buf_head++;
     buf_head %= CMD_BUF_SIZE;
 
-    submit_job(process_buffer[buf_head - 1]->cmd);
-
+    // ensure buffer is in accordance to current policy
     sort_buffer(process_buffer);
 
-    /* Unlock the shared command queue */
+    // print information about job
+    submit_job(process_buffer[buf_head - 1]->cmd);
 
+    /* Unlock the shared command queue */
     pthread_cond_signal(&cmd_buf_not_empty);
     pthread_mutex_unlock(&cmd_queue_lock);
 }
 
-char *get_policy_string()
-{
-
-    switch (policy)
-    {
-    case FCFS:
-        return "FCFS";
-
-    case SJF:
-        return "SJF";
-
-    case PRIORITY:
-        return "Priority";
-
-    default:
-        return "Unknown";
-    }
-}
-void submit_job(const char *cmd)
-{
-    const char *str_policy = get_policy_string();
-    printf("Job %s was submitted.\n", cmd);
-    printf("Total number of jobs in the queue: %d\n", buf_head - buf_tail);
-    printf("Expected waiting time: %d\n",
-           calculate_wait());
-    printf("Scheduling Policy: %s.\n", str_policy);
-}
-
-int calculate_wait()
-{
-    int wait = 0;
-    for (int i = buf_tail; i < buf_head; i++)
-    {
-        wait += process_buffer[i]->cpu_remaining_burst;
-    }
-    if (running_process != NULL)
-        wait += running_process->cpu_remaining_burst;
-    return wait;
-}
-
-process_p get_process(char **argv)
-{
-    process_p process = malloc(sizeof(process_t));
-    remove_newline(argv[3]);
-
-    // load process structure
-    strcpy(process->cmd, argv[1]);
-    process->arrival_time = time(NULL);
-    process->cpu_burst = atoi(argv[2]); // TODO
-    process->cpu_remaining_burst = process->cpu_burst;
-    process->priority = atoi(argv[3]); // TODO
-    process->interruptions = 0;
-    process->first_time_on_cpu = 0;
-    return process;
-}
-
-void skip_ahead(FILE *file, int index)
-{
-    int c = EOF;
-    int linecount = 0;
-    do
-    {
-        if (c == '\n')
-            linecount++;
-        if (linecount == index)
-            break;
-    } while ((c = fgetc(file)) != EOF);
-}
-
 /*
- * This function simulates a server running jobs in a batch mode.
- * Note: The input parameter (i.e., *ptr) is optional. 
- * If you intend to create a thread from a function 
- * with input parameters, please follow this example.
+ * Runs jobs on the process_buffer queue. After job is completed the job is sent to the finished_buffer queue
  */
 void *dispatcher(void *ptr)
 {
@@ -228,10 +152,56 @@ void *dispatcher(void *ptr)
         /* Move buf_tail forward, this is a circular queue */
         buf_tail++;
         buf_tail %= CMD_BUF_SIZE;
+
+        running_process = NULL;
     }
     return (void *)NULL;
 }
 
+/*
+ * Calculates the estimated wait time for the newly added process 
+ * to be loaded onto the CPU
+ */
+int calculate_wait()
+{
+    int wait = 0;
+    for (int i = buf_tail; i < buf_head; i++)
+    {
+        wait += process_buffer[i]->cpu_remaining_burst;
+    }
+    if (running_process != NULL)
+    {
+
+        // TODO: get a finer grain of estimation
+        // (time(NULL) - running_process->first_time_on_cpu) - cpu_remaining_burst
+        wait += running_process->cpu_remaining_burst;
+    }
+    return wait;
+}
+
+/*
+ * Loads process via argv, this is called when `run` is specified in the command line
+ */
+process_p get_process(char **argv)
+{
+    process_p process = malloc(sizeof(process_t));
+    remove_newline(argv[3]);
+
+    // load process structure
+    strcpy(process->cmd, argv[1]);
+    process->arrival_time = time(NULL);
+    process->cpu_burst = atoi(argv[2]);
+    process->cpu_remaining_burst = process->cpu_burst;
+    process->priority = atoi(argv[3]);
+    process->interruptions = 0;
+    process->first_time_on_cpu = 0;
+    return process;
+}
+
+/*
+ * Finishes a process. Runs the process using system and loads it into the
+ * finished_process buffer. Setting all the correct values as needed.
+ */
 void complete_process(process_p process)
 {
     char cmd[MAX_CMD_LEN * 2];
@@ -242,7 +212,6 @@ void complete_process(process_p process)
 
     system(cmd);
 
-    // int burst = run_process(process->cpu_remaining_burst);
     process->cpu_remaining_burst = 0;
 
     finished_process_p finished_process = malloc(sizeof(finished_process_t));
@@ -260,9 +229,14 @@ void complete_process(process_p process)
     finished_process_buffer[finished_head] = finished_process;
     finished_head++;
 
-    // free(process);
+    //free(process);
 }
 
+/*
+ * Reports job / process metrics. If no jobs are completed then notify user and return
+ * 
+ * Else dump all metrics per job and overall metrics
+ */
 void report_metrics()
 {
     if (!finished_head)
@@ -284,21 +258,7 @@ void report_metrics()
     int max_cpu_burst = INT_MIN;
     int min_cpu_burst = INT_MAX;
 
-    char str_policy[32];
-    switch (policy)
-    {
-    case FCFS:
-        strcpy(str_policy, "First Come First Serve");
-        break;
-    case SJF:
-        strcpy(str_policy, "Shortest Job First");
-        break;
-    case PRIORITY:
-        strcpy(str_policy, "Priority");
-        break;
-    }
-
-    printf("\n=== Reporting Metrics for %s ===\n\n", str_policy);
+    printf("\n=== Reporting Metrics for %s ===\n\n", get_policy_string());
     finished_process_p finished_process;
     int i = 0;
     for (; i < finished_head; i++)
@@ -366,6 +326,9 @@ void report_metrics()
     printf("\tMin CPU Burst:                  %d seconds\n\n", min_cpu_burst);
 }
 
+/*
+ * Sorts buffer per scheduling policy
+ */
 void sort_buffer(process_p *process_buffer)
 {
     void *sort;
@@ -380,11 +343,15 @@ void sort_buffer(process_p *process_buffer)
     case PRIORITY:
         sort = priority_scheduler;
     }
-    //todo REMOVE PROCESSES BEING RAN
 
+    // only sort buf_tail and more, if we sort at the base of process_buffer we will
+    // sort processes that are currently running on the cpu
     qsort(&process_buffer[buf_tail], buf_head - buf_tail, sizeof(process_p), sort);
 }
 
+/*
+ * SJF sorting algorithm used by qsort
+ */
 int sjf_scheduler(const void *a, const void *b)
 {
 
@@ -394,6 +361,9 @@ int sjf_scheduler(const void *a, const void *b)
     return (process_a->cpu_remaining_burst - process_b->cpu_remaining_burst);
 }
 
+/*
+ * FCFS sorting algorithm used by qsort
+ */
 int fcfs_scheduler(const void *a, const void *b)
 {
 
@@ -403,6 +373,9 @@ int fcfs_scheduler(const void *a, const void *b)
     return (process_a->arrival_time - process_b->arrival_time);
 }
 
+/*
+ * Priority Based sorting algorithm used by qsort
+ */
 int priority_scheduler(const void *a, const void *b)
 {
 
@@ -412,12 +385,11 @@ int priority_scheduler(const void *a, const void *b)
     return (process_a->priority - process_b->priority);
 }
 
-int run_process(int burst)
-{
-    sleep(burst);
-    return burst;
-}
-
+/*
+ * Utility function that removes new line from end of buffer
+ * 
+ * When using fgets or any other function the new line is part of the buffer
+ */
 void remove_newline(char *buffer)
 {
     int string_length = strlen(buffer);
@@ -427,15 +399,44 @@ void remove_newline(char *buffer)
     }
 }
 
+/*
+ * converts epoch time into a human readable format
+ */
 char *convert_time(time_t time)
 {
     return asctime(localtime(&time));
 }
 
-void print_process(process_p *process_buffer)
+/*
+ * Returns a human readbale string of the current policy
+ */
+char *get_policy_string()
 {
-    for (int i = 0; i < buf_head; i++)
+    switch (policy)
     {
-        printf("Process: %s, remaining burst: %d\n", process_buffer[i]->cmd, process_buffer[i]->cpu_remaining_burst);
+    case FCFS:
+        return "FCFS";
+
+    case SJF:
+        return "SJF";
+
+    case PRIORITY:
+        return "Priority";
+
+    default:
+        return "Unknown";
     }
+}
+
+/*
+ * After a job is submitted print information about the current queue
+ */
+void submit_job(const char *cmd)
+{
+    const char *str_policy = get_policy_string();
+    printf("Job %s was submitted.\n", cmd);
+    printf("Total number of jobs in the queue: %d\n", buf_head - buf_tail);
+    printf("Expected waiting time: %d\n",
+           calculate_wait());
+    printf("Scheduling Policy: %s.\n", str_policy);
 }
