9afd17a32823073411e12b2c6bc4a783c31cc0d7
Jordan Sosnowski <jjs0029@auburn.edu>
2020-03-07 20:26:45 -0600

    Update microbatch
    throwing error due to newline

--- src/microbatch.c
+++ src/microbatch.c
@@ -1,12 +1,27 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+void remove_newline(char *buffer)
+{
+    int string_length = strlen(buffer);
+    if (buffer[string_length - 1] == '\n')
+    {
+        buffer[string_length - 1] = '\0';
+    }
+}
 
 int main(int argc, char **argv)
 {
+
     if (argc != 2)
     {
         return 1;
     }
-    sleep(atoi(argv[2]));
+
+    remove_newline(argv[1]);
+
+    sleep(atoi(argv[1]));
+
     return 0;
 }
